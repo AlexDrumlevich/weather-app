c925bb2c7ae0800d382061ba3ef1360de6ffdc5a
Alexey Drumlevich <l-gc@yandex.ru>
2023-10-28 13:24:11 +0300

    promise examples

--- src/data/WeatherDataProcessor.js
+++ src/data/WeatherDataProcessor.js
@@ -71,3 +71,42 @@ constructor() {
        })
     }
 }
+
+/*
+async function displayTimesTemperatures(url) {
+    const response = await fetch(url);
+    const data =  await response.json();
+    console.log(data.hourly.time);
+    console.log(data.hourly.temperature_2m)
+}
+function displayTimesTemperaturesThens(url) {
+    const promiseResponse = fetch(url);
+    const promiseData = promiseResponse.then(response => response.json());
+    promiseData.then(data => {
+        console.log(data.hourly.time);
+        console.log(data.hourly.temperature_2m)
+    })
+}
+
+**********
+async function callMe() {
+    try {
+        console.log(1);
+        const p1 = crazyFunction(100000, "crazy1000");
+        const p2 = crazyFunction(2000, "crazy2000");
+        Promise.race([p1, p2]).then((messages) => console.log(messages));
+    } catch (error) {
+        console.log("error")
+    }
+
+}
+function crazyFunction(timeout, message) {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            
+           res(message)
+        }, timeout);
+    });
+}
+callMe();
+*/
